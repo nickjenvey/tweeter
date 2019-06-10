6c515e6afba947c7e40d94dd30b7d613c487fa9c
Nick <nbjenvey@gmail.com>
2019-06-10 23:11:51 +0000

    Fixed tab sizes back to 4 Spaces Per Tab Masterrace after Travis and Nima scared me

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  $(".new-tweet__textarea").on("input", function() {
-    let maxChars = 140;
-    let currentChars = $(this).val().length;
-    let remaining = maxChars - currentChars;
-    $(this).parent().find(".counter").text(remaining);
-    if (currentChars > maxChars) {
-      $(this).parent().find(".counter").css("color", "red");
-    } else {
-      $(this).parent().find(".counter").css("color", "#244751");
-    }
-  });
+    $(".new-tweet__textarea").on("input", function() {
+        let maxChars = 140;
+        let currentChars = $(this).val().length;
+        let remaining = maxChars - currentChars;
+        $(this).parent().find(".counter").text(remaining);
+        if (currentChars > maxChars) {
+        $(this).parent().find(".counter").css("color", "red");
+        } else {
+        $(this).parent().find(".counter").css("color", "#244751");
+        }
+    });
 });
