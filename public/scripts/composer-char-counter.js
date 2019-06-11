ccc6f476b9aa81070e61eb9d1c930d25001feca4
Nick <nbjenvey@gmail.com>
2019-06-11 00:54:31 +0000

    Refactored to simplify process

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
   $(".new-tweet__textarea").on("input", function() {
-    let maxChars = 140;
     let currentChars = $(this).val().length;
-    let remaining = maxChars - currentChars;
+    let remaining = 140 - currentChars;
     $(this).parent().find(".counter").text(remaining);
-    if (currentChars > maxChars) {
+    if (currentChars > 140) {
       $(this).parent().find(".counter").addClass("counter--negative");
     } else {
       $(this).parent().find(".counter").removeClass("counter--negative");
